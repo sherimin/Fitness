c0ee86d98be479cbf016e9c49be045c292a15ebf
Sheri Lu <sherisherilu@gmail.com>
2024-02-23 18:58:36 -0800

    Add AI images. Complete TailwindCSS configuration.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,42 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "berry-20": "FFE7E7",
+        "berry-50": "CAA6A6",
+        "berry-100": "B47B84",
+        "berry-500": "944E63",
+        "primary-100": "7E6363",
+        "primany-300": "503C3C",
+        "primary-500": "3E3232",
+        "secondary-400": "ECB159",
+        "secondary-500": "B67352",
+      },
+      backgroundImage: (theme) => ({
+        "gradient-pinkorange": "linear-gradient(90deg, #ECB159 0%, #FF8911 100%)",
+        "mobile-home": "url('./assets/HomePageGraphic.png')",
+      }),
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+        playfair: ["Playfair Display", "serif"]
+      },
+      content: {
+        evolvetext: "url(./assets/EvolveText.png",
+        abstractwaves: "url(./assets/AbstractWaves.png",
+        sparkles: "url(./assets/Sparkles.png",
+        circles: "url(./assets/Circles.png",
+      },
+    },
+    screens: {
+      xs: "480px",
+      sm: "768px",
+      md: "1060px",
+    },
   },
   plugins: [],
-}
+};
