e9089c175f97e788d3c3bdbe0abcfe7f3d38c48f
Sheri Lu <sherisherilu@gmail.com>
2024-02-25 12:27:04 -0800

    Add content for NavBar.

--- src/scenes/NavBar/index.tsx
+++ src/scenes/NavBar/index.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+// import Logo from "@/assets/Logo.png"
 
 type Props = {};
 
@@ -11,10 +12,33 @@ const NavBar = (props: Props) => {
         <div 
           className={`${flexBetween} fixed top-0 z-30 w-full py-6`}
         >
-            About us
+            <div className={`${flexBetween} mx-auto w-5/6`}>
+                <div className={`${flexBetween} w-full gap-16`}>
+                    {/* Top-Left */}
+                    {/* <img alt="logo" src={Logo} /> */}
+                    LOGO
 
+                    {/* Top-Right */}
+                    <div className={`${flexBetween} w-full`}>
+                        {/* NavBar Links */}
+                        <div className={`${flexBetween} gap-8 text-sm`}>
+                            <p>Home</p>
+                            <p>Benefits</p>
+                            <p>Classes & Trainings</p>
+                            <p>Contacts</p>
+                        </div>
+
+                        {/* Sign In and Become a Member */}
+                        <div className={`${flexBetween} gap-8`}>
+                            <p>Sign In</p>
+
+                            <button>Become a Member</button>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
-
     </nav>
   )
 }
