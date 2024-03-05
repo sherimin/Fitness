1f4bd2df5aec4c9409e291e589fa0f5c99dfef15
Sheri Lu <sherisherilu@gmail.com>
2024-03-04 23:21:22 -0800

    Add benefits details.

--- src/scenes/benefits/index.tsx
+++ src/scenes/benefits/index.tsx
@@ -13,45 +13,56 @@ type Props = {
 };
 
 const benefits: Array<BenefitType> = [
-    {
-        icon: <UserGroupIcon className="h-6 w-6" />,
-        title: "Personalized Coaching",
-        description: "Our expert coaches provide tailored guidance to help you maximize your workouts and reach your fitness goals efficiently."
-    },
-    {
-        icon: <GlobeAmericasIcon className="h-6 w-6" />,
-        title: "Remote Accessibility",
-        description: "With our remote coaching feature, you can access expert guidance and support from the comfort of your own home or while traveling, ensuring consistent progress no matter where you are."
-    },
-    {
-        icon: <BuildingStorefrontIcon className="h-6 w-6" />,
-        title: "Nutrition Guidance",
-        description: "Our platform offers personalized nutrition advice to optimize your overall health and wellness, ensuring a comprehensive approach to your fitness journey."
-    }
-]
+  {
+    icon: <UserGroupIcon className="h-6 w-6" />,
+    title: "Personalized Coaching",
+    description:
+      "Our expert coaches provide tailored guidance to help you maximize your workouts and reach your fitness goals efficiently.",
+  },
+  {
+    icon: <GlobeAmericasIcon className="h-6 w-6" />,
+    title: "Remote Accessibility",
+    description:
+      "With our remote coaching feature, you can access expert guidance and support from the comfort of your own home or while traveling, ensuring consistent progress no matter where you are.",
+  },
+  {
+    icon: <BuildingStorefrontIcon className="h-6 w-6" />,
+    title: "Nutrition Guidance",
+    description:
+      "Our platform offers personalized nutrition advice to optimize your overall health and wellness, ensuring a comprehensive approach to your fitness journey.",
+  },
+];
 
 const Benefits = ({ setSelectedPage }: Props) => {
   return (
-    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20">
+    <section id="benefits" className="mx-auto min-h-full w-5/6 py-20 text-primary-300">
       <motion.div
         onViewportEnter={() => setSelectedPage(SelectedPage.Benefits)}
       >
         {/* Header */}
         <div className="md:my-5 md:w-3/5">
-            <HText>
-                revolutionize your fitness journey.
-            </HText>
-            <p className="my-5 text-sm">
-                At <text className="font-semibold">iCoach Fitness Hub</text>, we blend cutting-edge technology with personalized coaching to help you achieve your goals. With a focus on holistic health and individualized programming, <text className="font-semibold">iCoach Fitness Hub</text> is not just a gym; it's your partner in transformation.
-            </p>
+          <HText>revolutionize your fitness journey.</HText>
+          <p className="my-5 text-sm">
+            At <text className="font-semibold">iCoach Fitness Hub</text>, we
+            blend cutting-edge technology with personalized coaching to help you
+            achieve your goals. With a focus on holistic health and
+            individualized programming,{" "}
+            <text className="font-semibold">iCoach Fitness Hub</text> is not
+            just a gym; it's your partner in transformation.
+          </p>
         </div>
 
         {/* Benefits */}
         <div className="md: flex items-center justify-between gap-8 mt-5">
-            {benefits.map((benefit) => (
-                <Benefit />
-            ))}
-
+          {benefits.map((benefit: BenefitType) => (
+            <Benefit 
+              key={benefit.title}
+              icon={benefit.icon}
+              title={benefit.title}
+              description={benefit.description}
+              setSelectedPage={setSelectedPage}
+            />
+          ))}
         </div>
       </motion.div>
     </section>
--- src/shared/Benefit.tsx
+++ src/shared/Benefit.tsx
@@ -1,11 +1,37 @@
-import React from 'react'
+import AnchorLink from "react-anchor-link-smooth-scroll";
+import { SelectedPage } from "./types";
 
-type Props = {}
+type Props = {
+    icon: JSX.Element;
+    title: string;
+    description: string;
+    setSelectedPage: (value: SelectedPage) => void;
+};
 
-const Benefit = (props: Props) => {
+const Benefit = ({ icon, title, description, setSelectedPage}: Props) => {
   return (
-    <div>Benefit</div>
-  )
-}
+    <div className="mt-5 rounded-md border-2 border-cream-50 px-5 py-16 text-center">
+        <div className="mb-4 flex justify-center">
+            <div className="rounded-full border-2 border-cream-100 bg-cream-20 p-4">
+                {icon}
+            </div>
+        </div>
 
-export default Benefit
+        <h4 className="font-bold">
+            {title}
+        </h4>
+        <p className="my-3">
+            {description}
+        </p>
+        <AnchorLink
+              className="text-sm font-bold text-primary-300 underline hover:no-underline cursor-pointer"
+              onClick={() => setSelectedPage(SelectedPage.Contacts)}
+              href={`#${SelectedPage.Contacts}`}
+            >
+              Learn More
+            </AnchorLink>
+    </div>
+  )
+};
+
+export default Benefit;
